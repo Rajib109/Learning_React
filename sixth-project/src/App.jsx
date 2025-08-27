f43155330d79ac0e39d3f231b64212046717329f
Shujaat Hussain <sujathussain426@gmail.com>
2025-08-27 16:21:35 +0530

    update components for fuctionality

--- sixth-project/src/App.jsx
+++ sixth-project/src/App.jsx
@@ -1,9 +1,32 @@
 import InputBox from "./components/inputbox";
+import useCurrencyinfo from "./hooks/usecurrencyinfo";
+import { useState } from "react";
 
 const BackgroundImage =
   "https://media.istockphoto.com/id/2216912014/photo/stock-market-crashes-ai-driven-trading-and-real-time-financial-analytics.jpg?s=2048x2048&w=is&k=20&c=fIvJEE0IHyn3oZNTDxqPIzLhkOcu26aFcnoa4dJjQzw=";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyinfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    const temp = from;
+    setFrom(to);
+    setTo(temp);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
   return (
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
@@ -16,27 +39,43 @@ function App() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              convert();
             }}
           >
             <div className="w-full mb-1">
-              <InputBox label="From" />
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
             </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
               >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              <InputBox label="To" />
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={to}
+                amountDisable
+              />
             </div>
             <button
               type="submit"
               className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
             >
-              Convert
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
         </div>
--- sixth-project/src/hooks/usecurrencyinfo.js
+++ sixth-project/src/hooks/usecurrencyinfo.js
@@ -1,13 +1,12 @@
-import { useEffect, useState, useState } from "react";
+import { useEffect, useState } from "react";
 
-function useCurrencyinfo(currency){
+function useCurrencyinfo(currency="usd"){
     const [data, setdata] = useState({});
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/${currency}`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((response) => response.json())
         .then((response) => {
             setdata(response[currency]);
-            console.log(data);
         });
     }, [currency]);
     return data;
