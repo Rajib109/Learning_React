838bed087e82e32340b33b6ddd7f2672a5164a63
Shujaat Hussain <sujathussain426@gmail.com>
2025-08-31 18:34:10 +0530

    Add About component and update routing in main.jsx

--- react-router-modern/src/main.jsx
+++ react-router-modern/src/main.jsx
@@ -1,7 +1,20 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home'
+
+const router =createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App/>}>
+            <Route path='' element={<Home/>}/>
+        </Route>
+    )
+)
+
 
 createRoot(document.getElementById('root')).render(
-    <App />  
+    <RouterProvider router={router}/>
 )
